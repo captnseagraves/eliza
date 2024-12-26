eda727e9cf0539f42c1d82fb64b20ac864414358
captnseagraves <k.s.seagraves@gmail.com>
2024-12-25 19:58:46 -0700

    browser client running for dinewell

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -26,9 +26,13 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:3000",
+                target: "http://localhost:8080",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+            "/agents": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
             },
         },
     },
